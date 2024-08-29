ae6cec0448f47ba505ac8d3f33e8e5707a702ffc
DINDART Guillaume <guillaume.dindart@inria.fr>
2024-08-29 11:57:35 +0200

    add packe to CI

---
+++ rawlatex/manifest-elementaryx-texlive.scm
@@ -0,0 +1,13 @@
+;; Ce qui suit est un « manifeste » équivalent à la ligne de commande que vous
+;; avez passée. Vous pouvez le sauvegarder dans un fichier qui vous pourrez
+;; passer aux commandes « guix » qui acceptent l'option « --manifest »
+;; (ou « -m »).
+
+(specifications->manifest
+  (list "texlive-scheme-basic"
+        "texlive-soul"
+        "texlive-todonotes"
+        "texlive-tikz-timing"
+        "texlive-wasysym"
+        "texlive-wasy"
+        "texlive-beamer"))
